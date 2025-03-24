e9deb71a92ca5b115a8cdc0266deb706329b4679
Abanoub Maher <abanoub.maher70@gmail.com>
2025-03-25 00:51:21 +0200

    create search repo

---
+++ lib/features/search/data/repos/search_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:pokedex/core/errors/failuers.dart';
+import 'package:pokedex/core/models/pokemon_model_hive.dart';
+
+abstract class SearchRepo {
+  Future<Either<Failure, PokemonModelHive>> searchPokemonById({
+    required int id,
+  });
+}
