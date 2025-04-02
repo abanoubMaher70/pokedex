515c368a3c8af879d08d995ee89e6e78c6976b11
Abanoub Maher <abanoub.maher70@gmail.com>
2025-04-02 23:28:15 +0200

    make the page view scroll vertical and fix search card navigation

--- lib/core/widgets/pokemon_card.dart
+++ lib/core/widgets/pokemon_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pokedex/core/constants/constant_colors.dart';
 import 'package:pokedex/core/models/pokemon_model/pokemon_model.dart';
 import 'package:pokedex/core/models/pokemon_model_hive.dart';
+import 'package:pokedex/core/utils/app_router.dart';
 import 'package:pokedex/core/utils/pokemon_utils.dart';
 import 'package:pokedex/core/utils/text_styles.dart';
 import 'package:pokedex/core/widgets/custom_cached_network_image.dart';
@@ -15,51 +17,57 @@ class PokemonCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final PokemonModel pokemon = pokemonHive.toPokemonModel();
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 14),
-      margin: const EdgeInsets.only(bottom: 16),
-      height: 135,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(14),
-        color: Color(pokemonHive.palette!),
-      ),
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                "#${pokemon.id.toString().padLeft(3, '0')}",
-                style: TextStyles.textStyle12.copyWith(
-                  color: ConstantColors.kWhiteTextColor,
+    return GestureDetector(
+      onTap:
+          () => GoRouter.of(
+            context,
+          ).push(AppRouter.kPokemonDetailsPageView, extra: pokemonHive),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 14),
+        margin: const EdgeInsets.only(bottom: 16),
+        height: 135,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(14),
+          color: Color(pokemonHive.palette!),
+        ),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "#${pokemon.id.toString().padLeft(3, '0')}",
+                  style: TextStyles.textStyle12.copyWith(
+                    color: ConstantColors.kWhiteTextColor,
+                  ),
                 ),
-              ),
-              Text(
-                pokemon.name!,
-                style: TextStyles.textStyle20.copyWith(
-                  color: ConstantColors.kWhiteTextColor,
+                Text(
+                  pokemon.name!,
+                  style: TextStyles.textStyle20.copyWith(
+                    color: ConstantColors.kWhiteTextColor,
+                  ),
                 ),
-              ),
-              PokemonTypeIcon(
-                pokemonType: convertPokemonType(
-                  pokemon.types!.first.type!.name!,
+                PokemonTypeIcon(
+                  pokemonType: convertPokemonType(
+                    pokemon.types!.first.type!.name!,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Positioned(
-            right: -20,
-            top: -5,
-            child: CustomCachedNetworkImage(
-              height: 155,
-              pokemonImage:
-                  pokemon.sprites!.other!.officialArtwork!.frontDefault!,
+              ],
             ),
-          ),
-        ],
+            Positioned(
+              right: -20,
+              top: -5,
+              child: CustomCachedNetworkImage(
+                height: 155,
+                pokemonImage:
+                    pokemon.sprites!.other!.officialArtwork!.frontDefault!,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/features/home/presentation/views/pokemon_details_view.dart
+++ lib/features/home/presentation/views/pokemon_details_view.dart
@@ -1,27 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:pokedex/core/constants/responsive_constant.dart';
-// import 'package:pokedex/core/models/pokemon_model_hive.dart';
-// import 'package:pokedex/features/home/presentation/views/widgets/pokemon_details_view_body.dart';
-
-// class PokemonDetailsView extends StatelessWidget {
-//   const PokemonDetailsView({super.key, required this.pokemonHive});
-
-//   final PokemonModelHive pokemonHive;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Color(pokemonHive.palette!),
-//       body: Padding(
-//         padding: ResponsiveConstant.pagePadding,
-//         child: SafeArea(
-//           child: PokemonDetailsViewBody(pokemonHive: pokemonHive),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:pokedex/core/models/pokemon_model_hive.dart';
 import 'package:pokedex/core/services/hive_service.dart';
@@ -47,9 +23,10 @@ class PokemonDetailsPageView extends StatelessWidget {
       initialPage: initialIndex,
     );
     return PageView.builder(
+      scrollDirection: Axis.vertical,
       controller: pageController,
       clipBehavior: Clip.none,
-      physics: const BouncingScrollPhysics(),
+      physics: const ClampingScrollPhysics(),
       itemBuilder: (context, index) {
         return PokemonDetailsViewBody(pokemonHive: pokemonHiveall[index]);
       },
